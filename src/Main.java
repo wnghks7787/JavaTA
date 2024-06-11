cd6fbf2a61d515f4d674e64d3e6b99e70189dbad
wnghks7787 <wnghks7787@naver.com>
2024-06-11 21:42:26 +0900

    Add 5 Shapes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import Shape.Shape;
+import Shape.Cycle;
+import Shape.Triangle;
+import Shape.Rectangle;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Shape> shapes = new ArrayList<>(5);
+
+        Shape c1 = new Cycle();
+        Shape t1 = new Triangle();
+        Shape r1 = new Rectangle();
+        Shape r2 = new Rectangle();
+        Shape c2 = new Cycle();
+
+        shapes.add(c1);
+        shapes.add(t1);
+        shapes.add(r1);
+        shapes.add(r2);
+        shapes.add(c2);
+
+        for(int i = 0 ; i < 5 ; i++) {
+
+        }
+
     }
 }
