56cf41ea4ee2801d2be070a5d716e88f643b9b57
wnghks7787 <wnghks7787@naver.com>
2024-06-11 20:48:07 +0900

    Add Shapes which implements shape interface

---
+++ src/Shape/Cycle.java
@@ -0,0 +1,27 @@
+package Shape;
+
+public class Cycle implements Shape {
+    private String name;
+    private double area;
+    private int perimeter;
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void printShape() {
+
+    }
+}
---
+++ src/Shape/Rectangle.java
@@ -0,0 +1,27 @@
+package Shape;
+
+public class Rectangle implements Shape {
+    private String name;
+    private double area;
+    private int perimeter;
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void printShape() {
+
+    }
+}
