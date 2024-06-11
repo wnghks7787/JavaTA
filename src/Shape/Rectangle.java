4fbba9bcb03818b76e54e87c69fad3d351bdb979
wnghks7787 <wnghks7787@naver.com>
2024-06-11 21:52:11 +0900

    Add setter

--- src/Shape/Rectangle.java
+++ src/Shape/Rectangle.java
@@ -4,6 +4,14 @@ public class Rectangle implements Shape {
     private double width;
     private double height;
 
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
     @Override
     public String getName() {
         return "Rectangle";
